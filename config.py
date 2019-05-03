18bb5425fa7b987534ff24f52775fefb54a7d27a
Vishal Harricharrna <vishal.harricharran@gmail.com>
2019-05-02 21:52:51 -0400

    Changed Requirements, Virtual Env, SQLite, Manage.py

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config(object):
+    pass
+class ProdConfig(Config):
+    pass
+class DevConfig(Config):
+    debug = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-import flask 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import DevConfig
+
+app = Flask(__name__)
+app.config.from_object(DevConfig)
+db =SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return '<h1>Hello World!</h1>'
+
+if __name__ == '__main__':
+    app.run()
+
+class User(db.Model):
+    __tablename__ ='user_table_name'
+    id = db.Column(db.Integer(), primary_key = True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+
+    def __init__(self, username):
+            self.username = username
+
+    def __repr__(self):
+        return "<User '{}'>".format(self.username)
