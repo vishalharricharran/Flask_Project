b968576a7ca1c0c3e388de690608e912041c4389
Vishal Harricharrna <vishal.harricharran@gmail.com>
2019-05-06 20:07:43 -0400

    Added Posts

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
-from main import app, db, User
+from main import app, db, User, Post
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db, User=User, Post=Post)
