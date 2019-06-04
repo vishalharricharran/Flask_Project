d9814c774e95a2d50e323d5a4fe182ac3dfa4af0
Vishal Harricharrna <vishal.harricharran@gmail.com>
2019-06-04 19:16:42 -0400

    Added functionality to DB

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
+import datetime
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import DevConfig
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
-db = SQLAlchemy(app)
 
-@app.route('/')
-def home():
-    return '<h1>Hello World!</h1>'
+db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     app.run()
 
+tags = db.Table(
+    'post_tags',
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+)
+
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255), nullable = False, unique = True)
     password = db.Column(db.String(255))
     posts = db.relationship('Post', backref = 'user', lazy = 'dynamic')
 
@@ -27,13 +32,50 @@ class User(db.Model):
 
 class Post(db.Model):
     id = db.Column(db.Integer(), primary_key = True)
-    title = db.Column(db.String(255))
+    title = db.Column(db.String(255), nullable = False)
     text = db.Column(db.Text())
-    publish_date = db.Column(db.DateTime())
+    publish_date = db.Column(db.DateTime(), default = datetime.datetime.now)
+    comments = db.relationship(
+        'Comment',
+        backref = 'post',
+        lazy = 'dynamic'
+    )
     user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    tags = db.relationship(
+        'Tag',
+        secondary = tags,
+        backref = db.backref('posts', lazy = 'dynamic')
+    )
 
     def __init__ (self, title):
         self.title = title
 
     def __repr__ (self):
-        return "<Post '{}'".format(self.title)
+        return "<Post '{}'>".format(self.title)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer(), primary_key = True)
+    name = db.Column(db.String(255), nullable = False)
+    text = db.Column(db.Text())
+    date = db.Column(db.DateTime(), default = datetime.datetime.now)
+    post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Comment '{}'>".format(self.text[:15])
+
+class Tag(db.Model):
+    id = db.Column(db.Integer(), primary_key = True)
+    title = db.Column(db.String(255), nullable = True, unique = True)
+
+    def __init__ (self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Tag '{}'>".format(self.title)
+
+@app.route('/')
+def home():
+    return '<h1>Hello World!</h1>'
--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
-from main import app, db, User, Post
+from main import app, db, User, Post, Comment, Tag
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Post=Post)
+    return dict(app=app, db=db, User=User, Post=Post, Comment=Comment, Tag=Tag)
